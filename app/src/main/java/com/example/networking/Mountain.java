ae62c90fdb5acb920dd53062ef55a44701b6877f
a22yifwa <a22yifwa@student.his.se>
2023-05-12 16:45:10 +0200

    Add constructors in Mountain.java

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,4 +1,20 @@
 package com.example.networking;
 
 public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    public Mountain(){
+        name="Saknar namn";
+        location="Saknar plats";
+        height=-1;
+    }
+
+    public Mountain(String n, String l, int h) {
+        name = n;
+        location = l;
+        height = h;
+    }
 }
+

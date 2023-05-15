c37e222c598e834ac15097d05ff509285c8f74c9
a22yifwa <a22yifwa@student.his.se>
2023-05-15 18:09:58 +0200

    Add new java class

---
+++ app/src/main/java/com/example/networking/RecyclerViewItem.java
@@ -0,0 +1,12 @@
+package com.example.networking;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewItem extends RecyclerView.ViewHolder{
+    public RecyclerViewItem(@NonNull View itemView) {
+        super(itemView);
+    }
+}

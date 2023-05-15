03be1edeb2b872a9318cfc65b18918bf83554c39
a22yifwa <a22yifwa@student.his.se>
2023-05-15 19:13:27 +0200

    kopplar ihop allting med RecyclerView Adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -22,12 +29,31 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        adapter = new RecyclerViewAdapter(this, listOfMountains, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         new JsonFile(this, this).execute(JSON_FILE);
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
     }
 
     @Override
     public void onPostExecute(String json) {
+
         Log.d("MainActivity", json);
+        Gson gson = new Gson(); // Create GSON object to perform marshall operations//
+        Type type = new TypeToken<List<Mountain>>() {}.getType();// Unmarshall JSON -> list of objects
+        List<Mountain> jsonMountains = gson.fromJson(json, type);
+        for(Mountain m : jsonMountains){
+            Log.d("MainActivityMountain", m.getName());
+            listOfMountains.add(new RecyclerViewItem(m.getName()));
+        }
+        adapter.notifyDataSetChanged();
     }
 
 }

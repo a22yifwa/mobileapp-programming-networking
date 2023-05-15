901d08de1cec5504f96c5905f329ab5ec0ee17ff
a22yifwa <a22yifwa@student.his.se>
2023-05-15 18:48:27 +0200

    Add RecyclerView Adapter

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.networking;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
+
+    ArrayList<RecyclerViewItem> listOfMountains= new ArrayList<>();
+    RecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
